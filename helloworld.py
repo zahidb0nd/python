19fdf88f379b0ccfbf5f866bad72fe6a2e4a5cf6
zahidb0nd <zahidbond7@gmail.com>
2024-08-23 11:28:28 +0530

    Initial commit

---
+++ helloworld.py
@@ -0,0 +1,5 @@
+print('hello world')
+a=5
+b='zahid'
+print(type(a))
+print(len(b))
---
+++ madlibs.py
@@ -0,0 +1,17 @@
+##string concatenation
+#
+#youtuber = "Zahid"
+#print("Subscribe to "+ youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+
+
+
+madlib = f"""Computer programming is so {adj}! it makes me so excited all the time because 
+i love to {verb1}. stay hydrated and {verb2} like you are {famous_person}!"""
+print(madlib)
