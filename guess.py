84cad3d3c09990b211455db590d91dadbacdadb4
zahidb0nd <zahidbond7@gmail.com>
2024-08-23 11:54:41 +0530

    a little guessing game in python

---
+++ guess.py
@@ -0,0 +1,14 @@
+import random
+
+def guess(x):
+    random_number = random.randint(1,x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f"Guess a number between 1 and {x}: "))
+        print(guess)
+        if guess <  random_number:
+            print("sorry, guess again. Too Low")
+        elif guess > random_number:
+            print("Sorry, guess again. Too High")
+    print(f"YAY!, congrats. you have guessed the number {random_number} correctly!!")
+guess(10)
