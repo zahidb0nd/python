bc0c30a2f2393cce7f8f447cad9500ca44709f83
zahidb0nd <zahidbond7@gmail.com>
2024-11-07 21:46:30 +0530

    lab program 1

---
+++ lab_programs/lists.py
@@ -0,0 +1,9 @@
+my_list = ['audi','lexus','nissan','bmw','toyota']
+print("original list: ",my_list)
+my_list.insert(2,'lamborghini')
+print("\nList after inserting at index 2: ",my_list)
+my_list.remove('audi')
+print("\nList after removing from index 0: ",my_list)
+my_list.append('ferrari')
+print("\nList after appending: ",my_list)
+
